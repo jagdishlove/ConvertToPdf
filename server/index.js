65d8d91b18ce3126ab3ff4defe13f7f39b4b6a8b
jagdishlove <jagdish.funn@gmail.com>
2021-11-17 21:59:09 +0530

    api-created

---
+++ server/index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const pdf = require('html-pdf');
+const cors = require('cors');
+
+const pdfTemplate = require('./documents');
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+//POST -PDF generation and featching of the data
+
+app.post('/create-pdf', (req, res) => {
+    pdf.create(pdfTemplate(req.body), {}).toFile('result.pdf', (err) => {
+        if (err) {
+            res.send(Promise.reject());
+
+
+        }
+        res.send(Promise.resolve());
+    });
+});
+
+
+//GET- SET generated PDF
+
+app.get('/fetch-pdf', (req, res) => {
+    res.sendFile(__dirname + '/result.pdf');
+})
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
