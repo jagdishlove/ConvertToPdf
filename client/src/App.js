51664a5ffde1fcc4455973fcf5ae34fbf7468b4f
jagdishlove <jagdish.funn@gmail.com>
2021-11-18 00:48:34 +0530

    Updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,42 +1,39 @@
-import './App.css';
-import { saveAs } from 'file-saver';
 import React, { useState } from 'react';
 import axios from 'axios';
+import { saveAs } from 'file-saver';
 
-function App() {
+import './App.css';
 
+const App = () => {
   const [state, setState] = useState({
-    name: " ",
+    name: '',
     receiptId: 0,
     price1: 0,
     price2: 0,
   });
 
+  const handleChange = ({ target: { value, name } }) => setState({ ...state, [name]: value });
 
-  const handleChange = ({ target: { value, name } }) => {
-    setState({ [name]: value });
-  }
-
-  const createDownloadPdf = () => {
+  const createAndDownloadPdf = () => {
     axios.post('/create-pdf', state)
-      .then(() => axios.get('fetch-pdf',{ responseType: 'blob' }))
-      .then(res=>{
+      .then(() => axios.get('fetch-pdf', { responseType: 'blob' }))
+      .then((res) => {
         const pdfBlob = new Blob([res.data], { type: 'application/pdf' });
-        saveAs(pdfBlob, 'result.pdf');
-      })
 
+        saveAs(pdfBlob, 'newPdf.pdf');
+      })
   }
 
   return (
     <div className="App">
       <input type="text" placeholder="Name" name="name" onChange={handleChange} />
-      <input type="number" placeholder="Receipt ID" name="ReceiptId" onChange={handleChange} />
-      <input type="number" placeholder="Price1" name="price1" onChange={handleChange} />
-      <input type="number" placeholder="Price2" name="price2" onChange={handleChange} />
-      <button onClick={createDownloadPdf}>Download PDF</button>
+      <input type="number" placeholder="Receipt ID" name="receiptId" onChange={handleChange} />
+      <input type="number" placeholder="Price 1" name="price1" onChange={handleChange} />
+      <input type="number" placeholder="Price 2" name="price2" onChange={handleChange} />
+      <button onClick={createAndDownloadPdf}>Download PDF</button>
     </div>
   );
-}
 
+}
 
 export default App;
